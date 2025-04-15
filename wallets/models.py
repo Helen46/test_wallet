cdde4bdc506122c7191ba35ef068def74eef2a80
Elena Prilutskay <a.priluckay87@gmail.com>
2025-04-16 01:13:53 +0300

    add model operation

--- wallets/models.py
+++ wallets/models.py
@@ -19,3 +19,31 @@ class Wallet(models.Model):
     class Meta:
         verbose_name = "Кошелек"
         verbose_name_plural = "Кошельки"
+
+
+class Operation(models.Model):
+    """
+    Модель операций
+    """
+    DEPOSIT = 'DEPOSIT'
+    WITHDRAW = 'WITHDRAW'
+    OPERATION_TYPES = [
+        (DEPOSIT, 'Deposit'),
+        (WITHDRAW, 'Withdraw'),
+    ]
+    wallet = models.ForeignKey(
+        Wallet,
+        on_delete=models.CASCADE,
+        related_name='operations'
+    )
+    operation_type = models.CharField(
+        max_length=8,
+        choices=OPERATION_TYPES
+    )
+    amount = models.DecimalField(
+        max_digits=20,
+        decimal_places=2
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
