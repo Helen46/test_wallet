fc311c08bc73b33d8609dabd964613dc32432148
Elena Prilutskay <a.priluckay87@gmail.com>
2025-04-16 01:02:26 +0300

    add model wallet

--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,21 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Wallet(models.Model):
+    """
+    Модель кошелька
+    """
+    uuid = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    balance = models.DecimalField(
+        max_digits=20,
+        decimal_places=2,
+        default=0
+    )
+
+    class Meta:
+        verbose_name = "Кошелек"
+        verbose_name_plural = "Кошельки"
