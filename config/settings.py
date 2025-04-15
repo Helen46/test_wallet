1d3bb6f6bceaf06e27c0690c3f6b0c056bf3e5ec
Elena Prilutskay <a.priluckay87@gmail.com>
2025-04-16 00:36:22 +0300

    add model user

--- config/settings.py
+++ config/settings.py
@@ -95,3 +95,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
+
+NULLABLE = {'blank': True, 'null': True}
--- users/models.py
+++ users/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from config.settings import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Электронная почта",
+        help_text="Укажите вашу электронную почту"
+    )
+    first_name = models.CharField(
+        max_length=50,
+        verbose_name="Имя пользователя",
+        help_text="Укажите имя"
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name="Фамилия пользователя",
+        help_text="Укажите фамилию"
+    )
+    phone = models.CharField(
+        max_length=35,
+        **NULLABLE,
+        verbose_name="Номер телефона",
+        help_text="Укажите ваш номер телефона"
+    )
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
