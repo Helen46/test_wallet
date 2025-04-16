59ad2d85e4d3dcc5a5c67357a1454500f01a9186
Elena Prilutskay <a.priluckay87@gmail.com>
2025-04-17 00:25:30 +0300

    add user create

--- users/urls.py
+++ users/urls.py
@@ -5,10 +5,12 @@ from rest_framework_simplejwt.views import (
 )
 
 from users.apps import UsersConfig
+from users.views import UserCreateAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path("register/", UserCreateAPIView.as_view(), name="register"),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
