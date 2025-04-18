dae148add33f6e7f30c21bc5858142dcac0d2709
Elena Prilutskay <a.priluckay87@gmail.com>
2025-04-19 01:21:19 +0300

    add urls for users

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import (
 )
 
 from users.apps import UsersConfig
-from users.views import UserCreateAPIView
+from users.views import UserCreateAPIView, UserListApiView, UserRetrieveApiView, UserUpdateApiView, UserDestroyApiView
 
 app_name = UsersConfig.name
 
@@ -14,4 +14,8 @@ urlpatterns = [
     path('register/', UserCreateAPIView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
+    path('list/', UserListApiView.as_view(), name='users_list'),
+    path('<int:pk>/', UserRetrieveApiView.as_view(), name='user_retrieve'),
+    path('<int:pk>/update', UserUpdateApiView.as_view(), name='user_update'),
+    path('<int:pk>/delete/', UserDestroyApiView.as_view(), name='user_delete'),
 ]
